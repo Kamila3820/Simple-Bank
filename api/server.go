27bf81262bd5c3e306c4efcd87027f76a1505682
thanadon.s <thanadonsaojarkaval@gmail.com>
2025-07-30 21:43:51 +0700

    - chore: init gin server

---
+++ api/server.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	db "simple-bank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts")
+
+	server.router = router
+	return server
+}
